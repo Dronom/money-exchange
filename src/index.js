95b6065c1630828b3f3055827fac2d2d7ad66cf3
Roman <dronymys@mail.ru>
2019-03-13 19:03:26 +0300

    feat:make a code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,52 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    let obj = {};
+    if(currency>10000) {
+        obj.error="You are rich, my friend! We don't have so much coins for exchange";
+        return obj;
+    }
+
+    if(currency<=0){
+        return obj;
+    }
+
+    if(currency>=50){
+        obj.H=0;
+    }
+    while(currency>=50){
+        obj.H++;
+        currency-=50;
+    }
+    if(currency>=25){
+        obj.Q=0;
+    }
+    while(currency>=25){
+        obj.Q++;
+        currency-=25;
+    }
+    if(currency>=10){
+        obj.D=0;
+    }
+    while(currency>=10){
+        obj.D++;
+        currency-=10;
+    }
+
+    if(currency>=5){
+        obj.N=0;
+    }
+    while(currency>=5){
+        obj.N++;
+        currency-=5;
+    }
+
+    if(currency>=1){
+        obj.P=0;
+    }
+
+    while(currency>=1){
+        obj.P++;
+        currency-=1;
+    }
+    return obj;
 }
